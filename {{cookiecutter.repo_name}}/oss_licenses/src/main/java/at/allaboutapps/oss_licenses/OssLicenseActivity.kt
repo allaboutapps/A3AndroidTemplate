23a3e3cc923932fdbb3227aaaa8036daebc95b34
Markus Gursch <markus.gursch@allaboutapps.at>
2019-09-19 15:51:40 +0200

    Add new module "oss_licenses" to setup license screen activity and dialog for all new apps

---
+++ {{cookiecutter.repo_name}}/oss_licenses/src/main/java/at/allaboutapps/oss_licenses/OssLicenseActivity.kt
@@ -0,0 +1,42 @@
+package at.allaboutapps.oss_licenses
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_oss.*
+
+class OssLicenseActivity : AppCompatActivity(R.layout.activity_oss) {
+
+    companion object {
+        fun showLicenses(context: Context, settings: LicenseScreenSettings) {
+            val intent = Intent(context, OssLicenseActivity::class.java)
+
+            intent.putExtra("settings", settings)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val settings = intent.extras?.getParcelable<LicenseScreenSettings>("settings")
+
+        settings?.let {
+            title = settings.title
+            supportActionBar?.setDisplayHomeAsUpEnabled(settings.showUpArrow)
+        }
+        web.loadUrl("file:///android_asset/open_source_licenses.html")
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
+
---
+++ {{cookiecutter.repo_name}}/oss_licenses/src/main/java/at/allaboutapps/oss_licenses/OssLicenseDialogFragment.kt
@@ -0,0 +1,39 @@
+package at.allaboutapps.oss_licenses
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import android.R
+import android.webkit.WebView
+import androidx.appcompat.app.AlertDialog
+
+
+class OssLicenseDialogFragment : DialogFragment() {
+
+    companion object {
+        fun showLicenseDialog(fragmentManager: FragmentManager, settings: LicenseScreenSettings) {
+            val fragment = OssLicenseDialogFragment()
+            val args = Bundle()
+            args.putParcelable("settings", settings)
+
+            fragment.arguments = args
+
+            fragmentManager.beginTransaction().add(fragment, "licenses_dialog").commit()
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val settings = arguments?.getParcelable<LicenseScreenSettings>("settings")
+
+        val webView = WebView(requireActivity())
+        webView.loadUrl("file:///android_asset/open_source_licenses.html")
+
+        return AlertDialog.Builder(requireActivity())
+                .setTitle(settings?.title)
+                .setView(webView)
+                .setPositiveButton(R.string.ok) { dialog, which -> dialog.dismiss() }
+                .create()
+    }
+}

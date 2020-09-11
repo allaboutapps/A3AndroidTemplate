ca41aa3699678a020515a8d56c0454f34adf3e77
Michael Federczuk <federczuk.michael@protonmail.com>
2020-09-11 14:31:59 +0200

    Update dependencies

--- {{cookiecutter.repo_name}}/networking/src/main/java/{{cookiecutter.package_name_dir}}/networking/services/ApiService.kt
+++ {{cookiecutter.repo_name}}/networking/src/main/java/{{cookiecutter.package_name_dir}}/networking/services/ApiService.kt
@@ -1,7 +1,7 @@
 package {{ cookiecutter.package_name }}.networking.services
 
 import {{ cookiecutter.package_name }}.networking.model.AuthToken
-import io.reactivex.Single
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.POST
 
 const val API_VERSION = "v1"

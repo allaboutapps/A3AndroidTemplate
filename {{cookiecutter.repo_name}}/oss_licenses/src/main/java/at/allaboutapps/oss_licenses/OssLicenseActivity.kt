41d0673a7c553b50a4c102511272d38d8f6f6db0
Luca Weiss <luca@z3ntu.xyz>
2021-01-29 13:09:03 +0100

    Migrate oss_licenses to viewbinding & some updates (#44)
    * Fix post_gen_project hook
    
    * Downgrade AGP to stable version
    
    * Update gradle to 6.8
    
    * Remove deprecated kotlin-android-extensions plugin
    
    * Update kotlin to 1.4.21
    
    * Migrate oss_licenses to viewbinding
    
    Co-authored-by: Luca Weiss <luca.weiss@allaboutapps.at>

--- {{cookiecutter.repo_name}}/oss_licenses/src/main/java/at/allaboutapps/oss_licenses/OssLicenseActivity.kt
+++ {{cookiecutter.repo_name}}/oss_licenses/src/main/java/at/allaboutapps/oss_licenses/OssLicenseActivity.kt
@@ -5,9 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_oss.*
+import at.allaboutapps.oss_licenses.databinding.ActivityOssBinding
 
-class OssLicenseActivity : AppCompatActivity(R.layout.activity_oss) {
+class OssLicenseActivity : AppCompatActivity() {
 
     companion object {
         private const val ARG_SETTINGS = "arg_settings"
@@ -19,15 +19,19 @@ class OssLicenseActivity : AppCompatActivity(R.layout.activity_oss) {
         }
     }
 
+    private lateinit var binding: ActivityOssBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityOssBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val settings = intent.extras!!.getParcelable<LicenseScreenSettings>(ARG_SETTINGS)!!
 
         title = settings.title
         supportActionBar?.setDisplayHomeAsUpEnabled(settings.showUpArrow)
 
-        web.loadWithSettings(GlobalWebviewSettings.prepareWebviewSettings())
+        binding.web.loadWithSettings(GlobalWebviewSettings.prepareWebviewSettings())
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {

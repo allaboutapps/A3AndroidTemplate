90bbf92cb16262461d29b557230342390edf1c12
Luca Weiss <luca@z3ntu.xyz>
2021-06-22 10:32:02 +0200

    FirebaseTokenService: fix imports (#47)
    Co-authored-by: Luca Weiss <luca.weiss@allaboutapps.at>

--- {{cookiecutter.repo_name}}/app/src/main/java/{{cookiecutter.package_name_dir}}/features/fcm/FirebaseTokenService.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/{{cookiecutter.package_name_dir}}/features/fcm/FirebaseTokenService.kt
@@ -1,7 +1,7 @@
 package {{ cookiecutter.package_name }}.features.fcm
 
-import io.reactivex.Single
-import com.test.networking.services.ApiService
+import io.reactivex.rxjava3.core.Single
+import {{ cookiecutter.package_name }}.networking.services.ApiService
 import javax.inject.Inject
 import javax.inject.Singleton
 

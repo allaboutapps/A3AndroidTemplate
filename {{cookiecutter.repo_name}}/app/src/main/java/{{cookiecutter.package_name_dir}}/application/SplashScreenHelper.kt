cace31ad108c64e45479812a38613249001ed8a8
Michael Federczuk <federczuk.michael@protonmail.com>
2021-08-21 22:09:11 +0200

    Bump up compile & target SDK version to 31

--- {{cookiecutter.repo_name}}/app/src/main/java/{{cookiecutter.package_name_dir}}/application/SplashScreenHelper.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/{{cookiecutter.package_name_dir}}/application/SplashScreenHelper.kt
@@ -16,13 +16,13 @@ internal class SplashScreenHelper : Application.ActivityLifecycleCallbacks {
     }
 
     // region unused overrides
-    override fun onActivityPaused(p0: Activity?) = Unit
+    override fun onActivityPaused(p0: Activity) = Unit
 
-    override fun onActivityResumed(p0: Activity?) = Unit
-    override fun onActivityStarted(p0: Activity?) = Unit
-    override fun onActivityDestroyed(p0: Activity?) = Unit
-    override fun onActivitySaveInstanceState(p0: Activity?, p1: Bundle?) = Unit
-    override fun onActivityStopped(p0: Activity?) = Unit
+    override fun onActivityResumed(p0: Activity) = Unit
+    override fun onActivityStarted(p0: Activity) = Unit
+    override fun onActivityDestroyed(p0: Activity) = Unit
+    override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) = Unit
+    override fun onActivityStopped(p0: Activity) = Unit
     // endregion
 
     companion object {

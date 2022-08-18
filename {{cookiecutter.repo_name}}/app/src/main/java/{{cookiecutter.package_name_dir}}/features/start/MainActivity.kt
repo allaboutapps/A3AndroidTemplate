ef582e73dab72769bcd91b82ee39b145be5d3887
Manuel Kubu <manuel.kubu@allaboutapps.at>
2022-08-18 13:57:31 +0200

    Add calling installSplashScreen in MainActivity

--- {{cookiecutter.repo_name}}/app/src/main/java/{{cookiecutter.package_name_dir}}/features/start/MainActivity.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/{{cookiecutter.package_name_dir}}/features/start/MainActivity.kt
@@ -1,6 +1,7 @@
 package {{ cookiecutter.package_name }}.features.start
 
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import {{ cookiecutter.package_name }}.R
 import {{ cookiecutter.package_name }}.base.BaseActivity
 import timber.log.Timber
@@ -10,6 +11,7 @@ class MainActivity : BaseActivity() {
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = viewModel()

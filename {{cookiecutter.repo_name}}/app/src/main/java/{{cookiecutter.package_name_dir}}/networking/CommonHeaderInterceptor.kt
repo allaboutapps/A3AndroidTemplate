8acc6e4943e1a904852afa7cf88cabbf55f46e95
David Medenjak <david.medenjak@allaboutapps.at>
2023-11-10 09:05:05 +0100

    Extend basic interceptor by `X-Client-Platform`

--- {{cookiecutter.repo_name}}/app/src/main/java/{{cookiecutter.package_name_dir}}/networking/CommonHeaderInterceptor.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/{{cookiecutter.package_name_dir}}/networking/CommonHeaderInterceptor.kt
@@ -9,9 +9,13 @@ import timber.log.Timber
 import javax.inject.Inject
 
 /**
- * Adds `User-Agent` and `Accept-Language` headers to every request.
+ * Adds following header to every request:
+ *
+ * * `User-Agent` with meta info about app and device versions
+ * * `Accept-Language` with the currently active language configuration
+ * * `X-Client-Platform` as a simple identifier for the client platform
  */
-class UserAgentInterceptor @Inject constructor(private val context: Context) : Interceptor {
+class CommonHeaderInterceptor @Inject constructor(private val context: Context) : Interceptor {
 
     private val userAgent: String
 
@@ -32,6 +36,7 @@ class UserAgentInterceptor @Inject constructor(private val context: Context) : I
             .newBuilder()
             .addHeader(HEADER_USER_AGENT, userAgent)
             .addHeader(HEADER_ACCEPT_LANGUAGE, readUserLanguages(context))
+            .addHeader(HEADER_X_PLATFORM, PLATFORM.lowercase())
             .build()
 
         return chain.proceed(request)
@@ -40,6 +45,7 @@ class UserAgentInterceptor @Inject constructor(private val context: Context) : I
     companion object {
         private const val HEADER_ACCEPT_LANGUAGE = "Accept-Language"
         private const val HEADER_USER_AGENT = "User-Agent"
+        private const val HEADER_X_PLATFORM = "X-Client-Platform"
 
         private const val PLATFORM = "Android"
 
